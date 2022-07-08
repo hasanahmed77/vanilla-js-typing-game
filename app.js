77372ea8475e077fe066110448c4c508b12348a8
Hasan Ahmed <megatron.kyuubi@gmail.com>
2022-07-09 03:39:07 +0600

    Added the timer

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const input = document.querySelector('input');
 const timer = document.querySelector('.timer');
 const words = document.querySelector('.words');
 
+let timerStart = false;
 let time = 0;
 
 input.addEventListener('input', e =>{
     console.log(e.target.value);
+    if(e.target.value.length >= 1) timerStart = true
+
+    console.log(timerStart)
 
     if(e.target.value.includes(' ')){
         console.log('reset')
@@ -13,3 +17,10 @@ input.addEventListener('input', e =>{
     }
 })
 
+setInterval(() => {
+    if(timerStart){
+        console.log(time)
+        timer.innerHTML= time;
+        time++
+    }
+}, 1000)
