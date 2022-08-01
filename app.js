ea2139f4e47e8a9b65f7d0d370f21abbad5c56d6
Hasan Ahmed <megatron.kyuubi@gmail.com>
2022-08-01 22:39:53 +0600

    added scoring functionality

--- app.js
+++ app.js
@@ -4,23 +4,64 @@ const words = document.querySelector('.words');
 
 let timerStart = false;
 let time = 0;
+let score = 0;
+let i = 0;
+let userInput = '';
+
+const WORDS= "If you want to increase the potato"
+
+words.innerText = WORDS;
+
+listOfWords = WORDS.split(' ');
 
 input.addEventListener('input', e =>{
-    console.log(e.target.value);
+    userInput = e.target.value.toLowerCase();
+
     if(e.target.value.length >= 1) timerStart = true
 
-    console.log(timerStart)
-
     if(e.target.value.includes(' ')){
+        if(userInput.slice() === (listOfWords[i].toLowerCase() + " ")){
+            console.log(listOfWords[i], 'LIST')
+            score++;
+            i++;
+            console.log(score, "SCORE")
+        } else {
+            score = score
+        }
         console.log('reset')
+        console.log(userInput + 'USERINPUT')
         e.target.value= ''
+        userInput = ''
+        // i++
+        console.log(i, 'I')
     }
 })
 
+//  TIMER
+
 setInterval(() => {
-    if(timerStart){
+    if(timerStart && time <= 60){
+        time++
         console.log(time)
         timer.innerHTML= time;
-        time++
+        
+        if(time === 60){
+            alert(`Your score: ${score}`);
+        }
     }
+    
 }, 1000)
+
+
+
+// COMPARING THE INPUT TO THE WORDS
+console.log(listOfWords)
+
+// while(time < 60){
+//     if(e.target.value === listOfWords[i]){
+//         score++;
+//     }
+//     i++;
+//     console.log(score);
+// }
+
