56bfc118d79523475487cd9fcc8ab7628d169044
Mustakim Ahmed Hasan <mustakimahmedhasan@Mustakims-MacBook-Air.local>
2024-01-14 01:33:22 +0600

    completed the project

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const input = document.querySelector('input');
 const timer = document.querySelector('.timer');
 const words = document.querySelector('.words');
+const current = document.querySelector('.current')
+const next = document.querySelector('.next')
+const button = document.querySelector('.btn')
+const answer = document.querySelector('.answer')
 
 let timerStart = false;
 let time = 0;
 let score = 0;
 let i = 0;
+let counter = 0;
 let userInput = '';
+let endOfArray = 10;
+let startOfArray = 0;
 
-const WORDS= "If you want to increase the potato"
+let WORDS= "If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance If you want to increase the potato and fight with dogs and cats in the rain we shall dance"
+// let NEXT_WORDS = "with dogs and cats in the rain we shall dance"
 
-words.innerText = WORDS;
 
 listOfWords = WORDS.split(' ');
+// listOfNextWords = NEXT_WORDS.split(' ')
+
+listOfWordsSpan = listOfWords.map((word, index) => `<span class =1>${word}</span>`);
+// listOfNextWordsSpan = listOfNextWords.map((word, index) => `<span class =1>${word}</span>`);
+
+
+
+current.innerHTML = listOfWordsSpan.slice(startOfArray,endOfArray).join(' ');
+// next.innerHTML = listOfNextWordsSpan.join(' ')
+console.log(listOfWordsSpan.slice(0,4).join(' '), 'SPAN')
+
 
 input.addEventListener('input', e =>{
     userInput = e.target.value.toLowerCase();
@@ -21,9 +39,25 @@ input.addEventListener('input', e =>{
 
     if(e.target.value.includes(' ')){
         if(userInput.slice() === (listOfWords[i].toLowerCase() + " ")){
+
             console.log(listOfWords[i], 'LIST')
+            listOfWordsSpan[i] = listOfWordsSpan[i].replace('1', 'passed')
+            console.log(listOfWordsSpan[i], "SPAn")
+            console.log(startOfArray, endOfArray)
+            current.innerHTML = listOfWordsSpan.slice(startOfArray,endOfArray).join(' ');
             score++;
+            counter++
             i++;
+
+            if(endOfArray != listOfWordsSpan.length){
+                if(counter === 4){
+                    startOfArray += 4
+                    endOfArray += 4;
+                    counter = 0
+                }
+            }
+
+
             console.log(score, "SCORE")
         } else {
             score = score
@@ -37,16 +71,24 @@ input.addEventListener('input', e =>{
     }
 })
 
+button.addEventListener('click', () => {
+    location.reload()
+})
+
+
 //  TIMER
 
-setInterval(() => {
+const TIMER = setInterval(() => {
     if(timerStart && time <= 60){
         time++
         console.log(time)
         timer.innerHTML= time;
         
         if(time === 60){
-            alert(`Your score: ${score}`);
+            // alert(`Your score: ${score}`);
+            answer.innerText = `Your Typing Speed: ${score} words per minute`
+            button.style.display = 'block'
+            clearInterval(TIMER)
         }
     }
     
